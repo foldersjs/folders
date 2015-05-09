9740d5847a5c9633ae601c0ca47f88f82fab5b85
Rooftrellen <CoderRooftrellen@gmail.com>
2015-05-09 12:13:27 +0800

    add gulp test

--- src/test/test-folders-local.js
+++ src/test/test-folders-local.js
@@ -4,68 +4,83 @@ var fs = require('fs');
 
 var local = new FoldersLocal();
 
-local.ls('.', function cb(files) {
-	console.log("\nls results:");
-	console.log(files);
+describe('test command ls for folders local', function() {
+	it('should ls the file data in current dir', function(done) {
+		local.ls('.', function cb(files) {
+			console.log("\nls results:");
+			console.log(files);
 
-	local.meta('.', files, function cb(results) {
-		console.log("\nmeta results:");
-		console.log(results);
-	});
-
-});
-
-local.cat({
-	shareId : "test-share-Id",
-	data : {
-		fileId : "test-folders-local.js"
-	}
-}, function cb(results) {
-	console.log("\ncat result:");
-	console.log(results);
-	console.log("\nfile data");
-	var stream = results.data;
-	stream.on('readable', function() {
-		var chunk;
-		var decoder = new StringDecoder('utf8');
-		while (null !== (chunk = stream.read())) {
-			console.log('\ngot %d bytes of data', chunk.length);
-			var strdata = decoder.write(chunk);
-			console.log('data:\n+' + strdata);
-		}
+			local.meta('.', files, function cb(results) {
+				console.log("\nmeta results:");
+				console.log(results);
+				done();
+			});
+		});
 	});
 });
 
-var fileNameToWrite = "./test.dat";
-local.write({
-	uri : fileNameToWrite,
-	streamId : "streamId",
-	data : (new Array(960 + 1)).join("Z"),
-	headers : {
-		"X-File-Date" : "2013-09-07T21:40:55.000Z",
-		"X-File-Name" : "stub-file.txt",
-		"X-File-Size" : "960",
-		"X-File-Type" : "text/plain"
-	},
-	shareId : "test-share-Id"
-}, function(result) {
-	console.log(result);
+describe('test command cat for folders local', function() {
+	it('should cat the file data in current dir', function(done) {
+		local.cat({
+			shareId : "test-share-Id",
+			data : {
+				fileId : "./gulpfile.js"
+			}
+		}, function cb(results) {
+			console.log("\ncat result:");
+			console.log(results);
+			console.log("\nfile data");
+			var stream = results.data;
+			stream.on('readable', function() {
+				var chunk;
+				var decoder = new StringDecoder('utf8');
+				while (null !== (chunk = stream.read())) {
+					console.log('\ngot %d bytes of data', chunk.length);
+					//var strdata = decoder.write(chunk);
+					//console.log('data:\n+' + strdata);
+				}
+				done();
+			});
+		});
+	});
+});
 
-	fs.exists(fileNameToWrite, function(exists) {
-		if (exists) {
+describe('test command put for folders local', function() {
+	it('should put the data to dest file in current dir', function(done) {
+		var fileNameToWrite = "./test.dat";
+		local.write({
+			uri : fileNameToWrite,
+			streamId : "streamId",
+			data : (new Array(960 + 1)).join("Z"),
+			headers : {
+				"X-File-Date" : "2013-09-07T21:40:55.000Z",
+				"X-File-Name" : "stub-file.txt",
+				"X-File-Size" : "960",
+				"X-File-Type" : "text/plain"
+			},
+			shareId : "test-share-Id"
+		}, function(result) {
+			console.log(result);
 
-			console.log("local file write success, file:" + fileNameToWrite);
-			local.ls(".", function cb(files) {
-				console.log("\nls the tmp test.data we upload:");
-				console.log(files);
+			fs.exists(fileNameToWrite, function(exists) {
+				if (exists) {
 
-				console.log("delete the tmp file we write");
-				fs.unlinkSync(fileNameToWrite);
+					console.log("local file write success, file:" + fileNameToWrite);
+					local.ls(".", function cb(files) {
+						console.log("\nls the tmp test.data we upload:");
+						console.log(files);
+
+						console.log("delete the tmp file we write");
+						fs.unlinkSync(fileNameToWrite);
+						done();
+					});
+
+				} else {
+					console.log("local file write failed, file:" + fileNameToWrite);
+					done();
+				}
 			});
 
-		} else {
-			console.log("local file write failed, file:" + fileNameToWrite);
-		}
+		});
 	});
-
 });
