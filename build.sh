cfc78ce7c77de567a7e9cc8f8c886d35191d826d
Timo Tervola <timo@tervola.com>
2015-07-19 22:44:57 +0700

    added build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#Copy this file to parent folder and run it
+npm install folders
+npm install folders-aws
+npm install folders-cli
+npm install folders-ftp
+npm install folders-gulp
+npm install folders-hdfs
+npm install folders-http
+npm install folders-pgkcloud
+npm install folders-ssh
+
+
